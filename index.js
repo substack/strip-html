cc9d4506cffca7e0fac83d6af63b0497760bbd0d
Devin Weaver <suki@tritarget.org>
2014-12-14 15:25:50 -0500

    Clean up code
    There was some repetition from the former conditionals. The refactor
    removes the repetition (DRY). Adding more block tags is easier now.

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ var tokenize = require('html-tokenize');
 var combine = require('stream-combiner2');
 var through = require('through2');
 
+var tagMatcher      = /^<([^ >]_[ >])/;
+var ignoreBlockTags = ['script', '!--'];
+
+function getTagName(str) {
+    return str.match(tagMatcher)[1];
+}
+
 module.exports = function () {
-    var tok = tokenize();
     var inside = false;
     var stream = through.obj(function (row, enc, next) {
-        var s = row[1].toString('utf8');
-        if (row[0] === 'open' && /^<script/i.test(s)) {
-            inside = 'script';
+        var tagName;
+        if (row[0] === 'open') {
+            tagName = getTagName(row[1].toString('utf8'));
+            inside  = (ignoreBlockTags.indexOf(tagName) >= 0);
         }
-        else if (row[0] === 'open' && /^<!--/.test(s)) {
-            inside = 'comment';
-        }
-        else if (inside) {
-            if (row[0] === 'close') inside = false;
+        else if (inside && row[0] === 'close') {
+            inside = false;
         }
         else if (row[0] === 'text') {
             this.push(row[1]);
